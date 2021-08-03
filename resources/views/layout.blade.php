45bd3f950ed5918eef01c5b8016bd6becb3d5a5d
Carlos Fernandez <saymonlenny@gmail.com>
2021-08-02 20:47:26 -0600

    diseño home

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,101 +1,191 @@
-
 <!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  
-   <!-- CSRF Token -->
-   <meta name="csrf-token" content="{{ csrf_token() }}">
-  
-    <title>HEROES VOLUNTARIOS</title>
+<html>
+<title>HEROES VOLUNTARIOS</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 
-
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-
-        <link href="/css/estilos.css" rel="stylesheet">
-
-</head>
 <body>
 
-<div id="app">
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">Heroes voluntarios</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-
-
-                    <li class="nav-item">
-                                <a class="nav-link"  href="/">Inicio</a>
-                        </li>
-                            
-                        <li class="nav-item">
-                            <a class="nav-link"  href="/layout_empresas">Empresas/ONG</a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link"  href="/layout_voluntarios">Voluntarios</a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link"  href="/quienesSomos">Quienes Somos</a>
-                        </li>
-
-                    </ul>
-
-                     <!-- Right Side Of Navbar -->
-                     <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Iniciar sesion') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Registrarse') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Salir') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
+<!-- Navbar (sit on top) -->
+<div class="w3-top">
+  <div class="w3-bar w3-white w3-wide w3-padding w3-card">
+    <a href="#home" class="w3-bar-item w3-button"><b>HEROES</b> VOLUNTARIOS</a>
+    <!-- Float links to the right. Hide them on small screens -->
+    <div class="w3-right w3-hide-small">
+      <a href="/layout_voluntarios" class="w3-bar-item w3-button">Voluntarios</a>
+      <a href="#quienesSomos" class="w3-bar-item w3-button">Quiénes somos</a>
+      <a href="#about" class="w3-bar-item w3-button">¿Tienes dudas?</a>
+      <a href="#contact" class="w3-bar-item w3-button">Contacta</a>
       
-        </div>
+    </div>
+  </div>
+</div>
 
-        <script src="https://unpkg.com/vue-resizable@1"></script>
-        <script src="https://cdn.socket.io/4.1.1/socket.io.min.js" integrity="sha384-cdrFIqe3RasCMNE0jeFG9xJHog/tgOVC1E9Lzve8LQN1g5WUHo0Kvk1mawWjxX7a" crossorigin="anonymous"></script>
-         <script src="{{ asset('js/app.js') }}"></script>
-         <script src="{{ asset('js/push.js') }}"></script>
-        @yield('contenidos')
+<!-- Header -->
+<header class="w3-display-container w3-content w3-wide" style="max-width:1500px;" id="home">
+  <img class="w3-image" src="img/fond.png" alt="Architecture" width="1500" height="800">
+  <div class="w3-display-middle w3-margin-top w3-center">
+    <h1 class="w3-xxlarge w3-text-white"><span class="w3-padding w3-black w3-opacity-min"><b>HEROES</b></span> <span class="w3-hide-small w3-text-light-grey">VOLUNTARIOS</span></h1>
+  </div>
+</header>
+
+<!-- Page content -->
+<div class="w3-content w3-padding" style="max-width:1564px">
+
+  <!-- Sección Informate -->
+  <div class="w3-container w3-padding-32" id="projects">
+    <h1 class="w3-xXlarge w3-text-orange"><b>Infórmate.</b></h1>
+    <hr style="width:50px;border:5px solid orange" class="w3-round">
+  </div>
+
+  <div class="w3-row-padding">
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container">
+        <div class="w3-display-topleft w3-black w3-padding">Ambiental</div>
+        <img src="img/ambiental.jpg" alt="House" style="width:100%">
+      </div>
+    </div>
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container">
+        <div class="w3-display-topleft w3-black w3-padding">Educativo</div>
+        <img src="img/educa.jpg" alt="House" style="width:100%">
+      </div>
+    </div>
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container">
+        <div class="w3-display-topleft w3-black w3-padding">Social</div>
+        <img src="img/social.png" alt="House" style="width:100%">
+      </div>
+    </div>
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container">
+        <div class="w3-display-topleft w3-black w3-padding">Protección civil</div>
+        <img src="img/PC.jpg" alt="House" style="width:100%">
+      </div>
+    </div>
+              <button type="submit" class="w3-button w3-block w3-padding-large w3-orange w3-margin-bottom">Toda la oferta informativa</button>
+
+  </div>
+
+
+  <!-- Sección Quienes Somos -->
+  <div class="w3-container w3-padding-32" id="quienesSomos">
+<h1 class="w3-xXlarge w3-text-orange"><b>Quiénes somos.</b></h1>
+    <hr style="width:50px;border:5px solid orange" class="w3-round">
+    
+    <p>Somos un equipo enfocado a crear una herramienta de ayuda cuyo objetivo es conectar a las empresas y organizaciones que ofrecen programas de
+voluntariado en El Salvador con jóvenes y adultos que puedan ser voluntarios, orientándolos mediante una plataforma informática en la cual tengan acceso a información
+de las problemáticas de los ciudadanos, que conozcan las áreas de voluntario que ofrecen las empresas y que puedan registrarse en los programas que estas ofrecen.
+
+	<h4>Misión - Visión</h4>
+    
+     Nuestra misión como empresa es realizar, diseñar software de calidad, para que
+    nuestros clientes tengan la certeza de que somos una empresa de confianza
+    con la cual ellos puedan confiarnos sus productos. <br> 
+
+    Como empresa tenemos como visión ser una entidad en la cual siempre este
+    familiarizada y realizando software con tecnologias nuevas que van surgiendo
+    esto con el unico fin de que nuestros clientes obtengan productos con tenologias
+    emergentes.
+    
+    </p>
+  </div>
+
+  <!-- Seccion de Nuestro Equipo-->
+<div class="w3-container w3-padding-64 w3-center" id="team">
+<h1 class="w3-xXlarge w3-text-orange"><b>NUESTRO EQUIPO</b></h1>
+
+
+<div class="w3-row"><br>
+
+<div class="w3-quarter">
+  <img src="img/avatar.png" alt="Boss" style="width:45%" class="w3-circle w3-hover-opacity">
+  <h3>Josue Torres</h3>
+  <p>Web Designer</p>
+</div>
+
+<div class="w3-quarter">
+  <img src="img/avatar.png" alt="Boss" style="width:45%" class="w3-circle w3-hover-opacity">
+  <h3>Jenny Flores</h3>
+  <p>Support</p>
+</div>
+
+<div class="w3-quarter">
+  <img src="img/avatar.png" alt="Boss" style="width:45%" class="w3-circle w3-hover-opacity">
+  <h3>Josselyn Carolina</h3>
+  <p>Boss man</p>
+</div>
+
+<div class="w3-quarter">
+  <img src="img/avatar.png" alt="Boss" style="width:45%" class="w3-circle w3-hover-opacity">
+  <h3>Carlos Fernández</h3>
+  <p>Fixer</p>
+</div>
+</div>
+</div>
+
+ <!-- Seccion Colabora con una ONG -->
+
+    <h1 class="w3-xXlarge w3-text-orange"><b>Colabora con una ONG.</b></h1>
+    <hr style="width:50px;border:5px solid orange" class="w3-round">
+    
+  <div class="w3-row-padding w3-grayscale">
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <img src="img/ongs.jpg" alt="John" style="width:100%">
+      <h3>ONG</h3>
+    
+      <p><button class="w3-button w3-light-grey w3-block">Ir a ONG</button></p>
+    </div>
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <img src="img/ongs.jpg" alt="Jane" style="width:100%">
+      <h3>ONG</h3>
+
+      <p><button class="w3-button w3-light-grey w3-block">Ir a ONG</button></p>
+    </div>
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <img src="img/ongs.jpg" alt="Mike" style="width:100%">
+      <h3>ONG</h3>
+      <p><button class="w3-button w3-light-grey w3-block">Ir a ONG</button></p>
+    </div>
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <img src="img/ongs.jpg" alt="Dan" style="width:100%">
+      <h3>ONG</h3>
+      <p><button class="w3-button w3-light-grey w3-block">Ir a ONG</button></p>
+    </div>
+      <button type="submit" class="w3-button w3-block w3-padding-large w3-orange w3-margin-bottom">Todas las ONG dónde colaborar</button>
+  </div>
+  
+
+
+  <!-- Contact Section -->
+  <div class="w3-container w3-padding-32" id="contact">
+    <h3 class="w3-border-bottom w3-border-light-grey w3-padding-16">Contact</h3>
+    <p>Lets get in touch and talk about your next project.</p>
+    <form action="/action_page.php" target="_blank">
+      <input class="w3-input w3-border" type="text" placeholder="Name" required name="Name">
+      <input class="w3-input w3-section w3-border" type="text" placeholder="Email" required name="Email">
+      <input class="w3-input w3-section w3-border" type="text" placeholder="Subject" required name="Subject">
+      <input class="w3-input w3-section w3-border" type="text" placeholder="Comment" required name="Comment">
+      <button class="w3-button w3-black w3-section" type="submit">
+        <i class="fa fa-paper-plane"></i> SEND MESSAGE
+      </button>
+    </form>
+  </div>
+  
+<!-- Image of location/map -->
+<div class="w3-container">
+  <img src="/w3images/map.jpg" class="w3-image" style="width:100%">
+</div>
+
+<!-- End page content -->
+</div>
+
+
+<!-- Footer -->
+<footer class="w3-center w3-black w3-padding-16">
+  <p><h3>HEROES VOLUNTARIOS</h3></p>
+</footer>
+
 </body>
 </html>
