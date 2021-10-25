15708cbea5ea46f0fb2b9b624033679110b34b96
Josue Torres <usis022519@ugb.edu.sv>
2021-10-25 09:36:42 -0600

    Nuevo cambio

--- chat/server.js
+++ chat/server.js
@@ -1,31 +1,4 @@
-/*console.log("Hola mundo desde NodeJS");
-
-let num1 = 5,
-    num2 = 6,
-    resp = num1+ num2;
-console.log("La respuesta es: ", resp);
-
-let generarTable = (ntable) =>{
-    for(let i=1; i<=10; i++){
-        console.log(`${ntable}*${i}=${ntable*i}`);
-    }
-}; 
-
-generarTable(8);
-generarTable(9); */
-
-
 const port = 3001;
-
-/*
-const http = require("http"),
-      server = http.createServer( (req, resp)=>{
-          resp.writeHead(200, {'content-type':'text/plain'});
-          resp.end("Hola mundo desde NodeJS");
-      } ).listen(port);
-
-console.log("NodeJS se esta ejecutando en el puerto", port);   
-*/
 
 const http = require("http").Server(),
       express = require("express"),
@@ -157,32 +130,3 @@ io.on('connection', socket=>{
         });
     });
 });
-
-/*app.use(cors());
-app.use(express.json());
-
-app.post("/chat",(req, resp)=>{
-    mongodb.connect(url,(err, client)=>{
-        if(err) console.log("No fue posible conectarse a la base de datos",err);
-        const db = client.db(dbname);
-        const result = db.collection("chat").insertOne(req.body).then((result)=>{
-            resp.send( result );
-        });
-    });
-    resp.send("Este es un saludo desde un servidor de node con el framework de express: "+ req.body.msg);
-});
-
-app.get("/historial",(req, resp)=>{
-    mongodb.connect(url,(err, client)=>{
-        if(err) console.log("No fue posible conectarse a la base de datos",err);
-        const db = client.db(dbname);
-        db.collection("chat").find().toArray((err, msgs)=> {
-            resp( msgs );
-        })
-    });
-});
-
-app.listen(port,()=>{
-    console.log("Ejecutandose nuestra app con express en el puerto", port);
-});
-*/
