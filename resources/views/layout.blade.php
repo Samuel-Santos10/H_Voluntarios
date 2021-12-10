30880066c4933f3df24369f7d9d4cf0604fdd8c8
Carlos Santos <saymonlenny@gmail.com>
2021-12-10 15:11:45 -0600

    modifi

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,199 +1,200 @@
 <!DOCTYPE html>
 <html lang="es">
-  <head>
-    <title>HEROES VOLUNTARIOS</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-  </head>
+
+<head>
+  <title>HEROES VOLUNTARIOS</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+</head>
 
 <body>
 
-<!-- Navbar (sit on top) -->
-<div class="w3-top">
-  <div class="w3-bar w3-white w3-wide w3-padding w3-card">
-    <a href="#home" class="w3-bar-item w3-button"><strong>HEROES</strong> VOLUNTARIOS</a>
-    <!-- Float links to the right. Hide them on small screens -->
-    <div class="w3-right w3-hide-small">
-      <a href="/layout_voluntarios" class="w3-bar-item w3-button">Voluntarios</a>
-      <a href="#quienesSomos" class="w3-bar-item w3-button">Quiénes somos</a>
-      <a href="/historias" class="w3-bar-item w3-button">Historias de voluntarios/as</a>
-      <a href="/mostrar" class="w3-bar-item w3-button">Beneficios y ayudas</a>
-      <a href="/Vforos" class="w3-bar-item w3-button">Comparte en el foro</a>
-      <a href="/layout_END" class="w3-bar-item w3-button">Necesitas ayuda</a>
-      
-    </div>
-  </div>
-</div>
+  <!-- Navbar (sit on top) -->
+  <div class="w3-top">
+    <div class="w3-bar w3-white w3-wide w3-padding w3-card">
+      <a href="#home" class="w3-bar-item w3-button"><strong>HEROES</strong> VOLUNTARIOS</a>
+      <!-- Float links to the right. Hide them on small screens -->
+      <div class="w3-right w3-hide-small">
+        <a href="/layout_voluntarios" class="w3-bar-item w3-button">Voluntarios</a>
+        <a href="#quienesSomos" class="w3-bar-item w3-button">Quiénes somos</a>
+        <a href="/historias" class="w3-bar-item w3-button">Historias de voluntarios/as</a>
+        <a href="/mostrar" class="w3-bar-item w3-button">Beneficios y ayudas</a>
+        <a href="/Vforos" class="w3-bar-item w3-button">Comparte en el foro</a>
+        <a href="/layout_END" class="w3-bar-item w3-button">Necesitas ayuda</a>
 
-<!-- Header -->
-<header class="w3-display-container w3-content w3-wide" style="max-width:1500px;" id="home">
-  <img class="w3-image" src="img/fond.png" alt="Architecture" width="1500" height="800">
-  <div class="w3-display-middle w3-margin-top w3-center">
-  <img src="img/log.png" alt="img" style="width:100%;max-width:1000px">
-  </div>
-</header>
-
-<!-- Page content -->
-<div class="w3-content w3-padding" style="max-width:1564px">
-
-  <!-- Sección Informate -->
-  <div class="w3-container w3-padding-32" id="projects">
-    <h1 class="w3-xXlarge w3-text-orange"><strong>Infórmate.</strong></h1>
-    <hr style="width:50px;border:5px solid orange" class="w3-round">
-  </div>
-
-  <div class="w3-row-padding">
-    <div class="w3-col l3 m6 w3-margin-bottom">
-      <div class="w3-display-container">
-        <div class="w3-display-topleft w3-black w3-padding">Ambiental</div>
-        <img src="img/ambiental.jpg" alt="House" style="width:100%">
       </div>
     </div>
-    <div class="w3-col l3 m6 w3-margin-bottom">
-      <div class="w3-display-container">
-        <div class="w3-display-topleft w3-black w3-padding">Educativo</div>
-        <img src="img/educa.jpg" alt="House" style="width:100%">
-      </div>
-    </div>
-    <div class="w3-col l3 m6 w3-margin-bottom">
-      <div class="w3-display-container">
-        <div class="w3-display-topleft w3-black w3-padding">Social</div>
-        <img src="img/social.png" alt="House" style="width:100%">
-      </div>
-    </div>
-    <div class="w3-col l3 m6 w3-margin-bottom">
-      <div class="w3-display-container">
-        <div class="w3-display-topleft w3-black w3-padding">Protección civil</div>
-        <img src="img/PC.jpg" alt="House" style="width:100%">
-      </div>
-    </div>
-    <button type="submit" class="w3-button w3-block w3-padding-large w3-orange w3-margin-bottom"><a href="/informate" class="w3-bar-item w3-button">Toda la oferta informativa</a></button>
-
   </div>
 
+  <!-- Header -->
+  <header class="w3-display-container w3-content w3-wide" style="max-width:1500px;" id="home">
+    <img class="w3-image" src="img/fond.png" alt="Architecture" width="1500" height="800">
+    <div class="w3-display-middle w3-margin-top w3-center">
+      <img src="img/log.png" alt="img" style="width:100%;max-width:1000px">
+    </div>
+  </header>
 
-  <!-- Sección Quienes Somos -->
-  <div class="w3-container w3-padding-32" id="quienesSomos">
-<h1 class="w3-xXlarge w3-text-orange"><strong>Quiénes somos.</strong></h1>
-    <hr style="width:50px;border:5px solid orange" class="w3-round">
-    
-    <p>Somos un equipo enfocado a crear una herramienta de ayuda cuyo objetivo es conectar a las empresas y organizaciones que ofrecen programas de
-voluntariado en El Salvador con jóvenes y adultos que puedan ser voluntarios, orientándolos mediante una plataforma informática en la cual tengan acceso a información
-de las problemáticas de los ciudadanos, que conozcan las áreas de voluntario que ofrecen las empresas y que puedan registrarse en los programas que estas ofrecen.
+  <!-- Page content -->
+  <div class="w3-content w3-padding" style="max-width:1564px">
 
-	<h4>Misión - Visión</h4>
-    
-     Nuestra misión como empresa es realizar, diseñar software de calidad, para que
-    nuestros clientes tengan la certeza de que somos una empresa de confianza
-    con la cual ellos puedan confiarnos sus productos. <br> 
+    <!-- Sección Informate -->
+    <div class="w3-container w3-padding-32" id="projects">
+      <h1 class="w3-xXlarge w3-text-orange"><strong>Infórmate.</strong></h1>
+      <hr style="width:50px;border:5px solid orange" class="w3-round">
+    </div>
 
-    Como empresa tenemos como visión ser una entidad en la cual siempre este
-    familiarizada y realizando software con tecnologias nuevas que van surgiendo
-    esto con el unico fin de que nuestros clientes obtengan productos con tenologias
-    emergentes.
-    
-    </p>
-  </div>
+    <div class="w3-row-padding">
+      <div class="w3-col l3 m6 w3-margin-bottom">
+        <div class="w3-display-container">
+          <div class="w3-display-topleft w3-black w3-padding">Ambiental</div>
+          <img src="img/ambiental.jpg" alt="House" style="width:100%">
+        </div>
+      </div>
+      <div class="w3-col l3 m6 w3-margin-bottom">
+        <div class="w3-display-container">
+          <div class="w3-display-topleft w3-black w3-padding">Educativo</div>
+          <img src="img/educa.jpg" alt="House" style="width:100%">
+        </div>
+      </div>
+      <div class="w3-col l3 m6 w3-margin-bottom">
+        <div class="w3-display-container">
+          <div class="w3-display-topleft w3-black w3-padding">Social</div>
+          <img src="img/social.png" alt="House" style="width:100%">
+        </div>
+      </div>
+      <div class="w3-col l3 m6 w3-margin-bottom">
+        <div class="w3-display-container">
+          <div class="w3-display-topleft w3-black w3-padding">Protección civil</div>
+          <img src="img/PC.jpg" alt="House" style="width:100%">
+        </div>
+      </div>
+      <button type="submit" class="w3-button w3-block w3-padding-large w3-orange w3-margin-bottom"><a
+          href="/layout_informate_contenidos" class="w3-bar-item w3-button">Toda la oferta informativa</a></button>
 
-  <!-- Seccion de Nuestro Equipo-->
-<div class="w3-container w3-padding-64 w3-center" id="team">
-<h1 class="w3-xXlarge w3-text-orange"><strong>NUESTRO EQUIPO</strong></h1>
+    </div>
 
 
-<div class="w3-row"><br>
+    <!-- Sección Quienes Somos -->
+    <div class="w3-container w3-padding-32" id="quienesSomos">
+      <h1 class="w3-xXlarge w3-text-orange"><strong>Quiénes somos.</strong></h1>
+      <hr style="width:50px;border:5px solid orange" class="w3-round">
 
-<div class="w3-quarter">
-  <img src="img/avatar.png" alt="Boss" style="width:45%" class="w3-circle w3-hover-opacity">
-  <h3>Josue Torres</h3>
-  <p>Web Designer</p>
-</div>
+      <p>Somos un equipo enfocado a crear una herramienta de ayuda cuyo objetivo es conectar a las empresas y
+        organizaciones que ofrecen programas de
+        voluntariado en El Salvador con jóvenes y adultos que puedan ser voluntarios, orientándolos mediante una
+        plataforma informática en la cual tengan acceso a información
+        de las problemáticas de los ciudadanos, que conozcan las áreas de voluntario que ofrecen las empresas y que
+        puedan registrarse en los programas que estas ofrecen.
 
-<div class="w3-quarter">
-  <img src="img/avatar.png" alt="Boss" style="width:45%" class="w3-circle w3-hover-opacity">
-  <h3>Jenny Flores</h3>
-  <p>Support</p>
-</div>
+      <h4>Misión - Visión</h4>
 
-<div class="w3-quarter">
-  <img src="img/avatar.png" alt="Boss" style="width:45%" class="w3-circle w3-hover-opacity">
-  <h3>Josselyn Carolina</h3>
-  <p>Boss man</p>
-</div>
+      Nuestra misión como empresa es realizar, diseñar software de calidad, para que
+      nuestros clientes tengan la certeza de que somos una empresa de confianza
+      con la cual ellos puedan confiarnos sus productos. <br>
 
-<div class="w3-quarter">
-  <img src="img/avatar.png" alt="Boss" style="width:45%" class="w3-circle w3-hover-opacity">
-  <h3>Carlos Fernández</h3>
-  <p>Fixer</p>
-</div>
-</div>
-</div>
+      Como empresa tenemos como visión ser una entidad en la cual siempre este
+      familiarizada y realizando software con tecnologias nuevas que van surgiendo
+      esto con el unico fin de que nuestros clientes obtengan productos con tenologias
+      emergentes.
 
- <!-- Seccion Colabora con una ONG -->
+      </p>
+    </div>
+
+    <!-- Seccion de Nuestro Equipo-->
+    <div class="w3-container w3-padding-64 w3-center" id="team">
+      <h1 class="w3-xXlarge w3-text-orange"><strong>NUESTRO EQUIPO</strong></h1>
+
+
+      <div class="w3-row"><br>
+
+        <div class="w3-quarter">
+          <img src="img/avatar.png" alt="Boss" style="width:45%" class="w3-circle w3-hover-opacity">
+          <h3>Josue Torres</h3>
+          <p>Web Designer</p>
+        </div>
+
+        <div class="w3-quarter">
+          <img src="img/avatar.png" alt="Boss" style="width:45%" class="w3-circle w3-hover-opacity">
+          <h3>Jenny Flores</h3>
+          <p>Support</p>
+        </div>
+
+        <div class="w3-quarter">
+          <img src="img/avatar.png" alt="Boss" style="width:45%" class="w3-circle w3-hover-opacity">
+          <h3>Josselyn Carolina</h3>
+          <p>Boss man</p>
+        </div>
+
+        <div class="w3-quarter">
+          <img src="img/avatar.png" alt="Boss" style="width:45%" class="w3-circle w3-hover-opacity">
+          <h3>Carlos Fernández</h3>
+          <p>Fixer</p>
+        </div>
+      </div>
+    </div>
+
+    <!-- Seccion Colabora con una ONG -->
 
     <h1 class="w3-xXlarge w3-text-orange"><strong>Colabora con una ONG.</strong></h1>
     <hr style="width:50px;border:5px solid orange" class="w3-round">
-    
-  <div class="w3-row-padding w3-grayscale">
-    <div class="w3-col l3 m6 w3-margin-bottom">
-      <img src="img/ongs.jpg" alt="John" style="width:100%">
-      <h3>ONG</h3>
-    
-      <p><button class="w3-button w3-light-grey w3-block">Ir a ONG</button></p>
-    </div>
-    <div class="w3-col l3 m6 w3-margin-bottom">
-      <img src="img/ongs.jpg" alt="Jane" style="width:100%">
-      <h3>ONG</h3>
 
-      <p><button class="w3-button w3-light-grey w3-block">Ir a ONG</button></p>
-    </div>
-    <div class="w3-col l3 m6 w3-margin-bottom">
-      <img src="img/ongs.jpg" alt="Mike" style="width:100%">
-      <h3>ONG</h3>
-      <p><button class="w3-button w3-light-grey w3-block">Ir a ONG</button></p>
-    </div>
-    <div class="w3-col l3 m6 w3-margin-bottom">
-      <img src="img/ongs.jpg" alt="Dan" style="width:100%">
-      <h3>ONG</h3>
-      <p><button class="w3-button w3-light-grey w3-block">Ir a ONG</button></p>
-    </div>
-    <button type="submit" class="w3-button w3-block w3-padding-large w3-orange w3-margin-bottom"><a href="/layout_empresas" class="w3-bar-item w3-button">Todas las ONG dónde colaborar</a></button>
+    <div class="w3-row-padding w3-grayscale">
+      <div class="w3-col l3 m6 w3-margin-bottom">
+        <img src="img/ongs.jpg" alt="John" style="width:100%">
+        <h3>ONG</h3>
+
+        <p><button class="w3-button w3-light-grey w3-block">Ir a ONG</button></p>
+      </div>
+      <div class="w3-col l3 m6 w3-margin-bottom">
+        <img src="img/ongs.jpg" alt="Jane" style="width:100%">
+        <h3>ONG</h3>
+
+        <p><button class="w3-button w3-light-grey w3-block">Ir a ONG</button></p>
+      </div>
+      <div class="w3-col l3 m6 w3-margin-bottom">
+        <img src="img/ongs.jpg" alt="Mike" style="width:100%">
+        <h3>ONG</h3>
+        <p><button class="w3-button w3-light-grey w3-block">Ir a ONG</button></p>
+      </div>
+      <div class="w3-col l3 m6 w3-margin-bottom">
+        <img src="img/ongs.jpg" alt="Dan" style="width:100%">
+        <h3>ONG</h3>
+        <p><button class="w3-button w3-light-grey w3-block">Ir a ONG</button></p>
+      </div>
+      <button type="submit" class="w3-button w3-block w3-padding-large w3-orange w3-margin-bottom"><a
+          href="/layout_empresas" class="w3-bar-item w3-button">Todas las ONG dónde colaborar</a></button>
 
 
+    </div>
+
+
+    <!-- End page content -->
   </div>
-  
-<!-- Image of location/map -->
-<div class="w3-container">
-  <img src="/w3images/map.jpg" class="w3-image" alt="imags" style="width:100%">
-</div>
-
-<!-- End page content -->
-</div>
 
 
-<!-- Footer -->
-<footer class="w3-center w3-black w3-padding-16">
-  <p><h3>HEROES VOLUNTARIOS</h3></p>
-</footer>
+  <!-- Footer -->
+  <footer class="w3-center w3-black w3-padding-16">
+    <p>
+    <h3>HEROES VOLUNTARIOS</h3>
+    </p>
+  </footer>
 
-       <!-- Implementacion del chatBot en nuestro proyecto -->
-    <script src="https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1"></script>
-    <df-messenger
-        intent="WELCOME"
-        chat-title="Mensajeria_HV"
-        agent-id="a68ba817-b98c-4ebd-9872-3e056512fe95"
-        language-code="es"
-    ></df-messenger>
+  <!-- Implementacion del chatBot en nuestro proyecto -->
+  <script src="https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1"></script>
+  <df-messenger intent="WELCOME" chat-title="Mensajeria_HV" agent-id="a68ba817-b98c-4ebd-9872-3e056512fe95"
+    language-code="es"></df-messenger>
 </body>
+
 </html>
 
 <style>
-  df-messenger {
-   --df-messenger-bot-message: #0000FF;
-   --df-messenger-button-titlebar-color: #df9b56;
-   --df-messenger-chat-background-color: #fafafa;
-   --df-messenger-font-color: white;
-   --df-messenger-send-icon: #0000FF;
-   --df-messenger-user-message: #479b3d;
-  }
+df-messenger {
+  --df-messenger-bot-message: #0000FF;
+  --df-messenger-button-titlebar-color: #df9b56;
+  --df-messenger-chat-background-color: #fafafa;
+  --df-messenger-font-color: white;
+  --df-messenger-send-icon: #0000FF;
+  --df-messenger-user-message: #479b3d;
+}
 </style>
